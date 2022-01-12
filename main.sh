f11faf1afc085a618fdbb4a591094c9c596e8772
padmashree12 <padmashreejha717@gmail.com>
2022-01-12 05:12:15 +0000

    DONE

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
-# curl -s https://raw.githubusercontent.com/Taguar258/Raven-Storm/master/install.sh | sudo bash -s
+curl -s https://raw.githubusercontent.com/Taguar258/Raven-Storm/master/install.sh | sudo bash -s
 
 sudo rst < inputs.txt
